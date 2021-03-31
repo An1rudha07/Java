46dfb39a12fea492ba70c9d36faeb8a4bc18e3d5
Anirudha Upadhyay <81645498+An1rudha07@users.noreply.github.com>
2021-03-31 18:16:07 +0530

    Multiply two floating  numbers

---
+++ src/P03.java
@@ -0,0 +1,12 @@
+public class P03 {
+    //Multiply two floating integer number
+    public static void main(String[]arg) {
+        float first = 8.5f;
+        float second = 2f;
+        float product = first*second;
+        System.out.println("the product is :" + product);
+
+
+
+    }
+}
