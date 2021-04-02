a815111b070f84dc059ef454d7e0e58192b806c6
Anirudha Upadhyay <81645498+An1rudha07@users.noreply.github.com>
2021-04-02 23:59:00 +0530

    Compute Quotient and Remainder

---
+++ src/P05.java
@@ -0,0 +1,12 @@
+public class P05 {
+    //Compute Quotient and Remainder
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 7;
+        int quotient = a / b;
+        int remainder = a % b;
+
+        System.out.println("Quotient =" + quotient);
+        System.out.println("reminder =" + remainder);
+    }
+}
