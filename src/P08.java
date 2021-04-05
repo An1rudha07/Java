d33f8386c50f5cd8610701cac72034a6bba87679
Anirudha Upadhyay <81645498+An1rudha07@users.noreply.github.com>
2021-04-05 22:48:25 +0530

    Check weather an alphabet is Vowel or consonant

---
+++ src/P08.java
@@ -0,0 +1,13 @@
+public class P08 {
+    //Check weather an alphabet is Vowel or consonant
+    public static void main(String[] args) {
+        char ch = 'o';
+
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+            System.out.println(ch + " is vowel");
+        else
+            System.out.println(ch + " is consonant");
+
+    }
+}
+
